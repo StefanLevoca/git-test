865e4b20895ef5fd0cbca5bbcfe96b8faa61b018
student <student@158.197.35.88>
2018-09-19 09:01:22 +0200

    Komentar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,8 +11,12 @@ public class HelloWorld {
 			System.out.println();
 			//este komentar
 			
+			while (true) {
+				System.out.println("nekonecno");
+				
+			}
 			
-			System.out.println("Pridanie do projektu");
+					
 		}
 
 	}
