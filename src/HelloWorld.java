735eaf71b6cb540be8fa3229e2bd1707278bc27d
student <student@158.197.35.88>
2018-09-19 08:04:42 +0200

    pridanie komentara

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,8 +5,10 @@ public class HelloWorld {
 		
 		System.out.println("Hello world");
 		
+		//toto je dolezity komentar
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Git je super");
+			
 		}
 
 	}
